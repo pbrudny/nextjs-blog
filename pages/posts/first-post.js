8b9e84753c4d023bf07d51e7cb6be82777d20ab7
Piotr Brudny <pbrudny@gmail.com>
2024-05-18 00:12:20 +0200

    Add alert

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,23 +1,17 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-    <><Head>
-        <title>First Post</title>
-    </Head>
-    <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnLoad"
-        onLoad={() =>
-            console.log(`script loaded correctly, window.FB has been populated`)
-        }
-    />
+    <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
         <h1>First Post</h1>
         <h2>
             <Link href="/">Back to home</Link>
         </h2>
-    </>
-    )
+    </Layout>
+    );
 }
